026249a64122cd1941c69f43900d56baa9b17f4d
Hannah Baker <19.hannah.baker@ksd.org>
2019-02-06 09:05:57 -0800

    Level 2 start

---
+++ GroupMobile/Assets/Scripts/MobileHealth.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MobileHealth : MonoBehaviour
+{
+//If player collides w/object tagged Enemy, active scene will reload
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+}
