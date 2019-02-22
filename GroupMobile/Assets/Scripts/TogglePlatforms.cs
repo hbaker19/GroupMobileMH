2c9fcc15b05dbf2527ee73dbcd1be351551fc441
Hannah Baker <19.hannah.baker@ksd.org>
2019-02-22 08:46:07 -0800

    Level3 hardmode

---
+++ GroupMobile/Assets/Scripts/TogglePlatforms.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TogglePlatforms : MonoBehaviour
+{
+    //Will set any object in the array to active state
+    public GameObject[] platforms;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "PlatformAppear")
+        {
+            for (int i = 0; i < platforms.Length; i++)
+            {
+                (platforms[i]).SetActive(true);
+            }
+        }
+    }
+}
