0acf06c805df8163ea4dbcddbf85063a7929e0fc
Hannah Baker <19.hannah.baker@ksd.org>
2019-03-04 10:01:48 -0800

    Adding playtesting tag and script so player death will reload scene immediately

---
+++ GroupMobile/Assets/Scripts/Playtesting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playtesting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //This tag is for playtesting ONLY. Change before building
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Playtest")
+        {
+
+        }
+    }
+}
