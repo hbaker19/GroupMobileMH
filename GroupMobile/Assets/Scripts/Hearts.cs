d0568f55dafe4765223f7a46941986be77031da8
Hannah Baker <19.hannah.baker@ksd.org>
2019-03-07 09:17:01 -0800

    Hearts and level portals now work as triggers.

--- GroupMobile/Assets/Scripts/Hearts.cs
+++ GroupMobile/Assets/Scripts/Hearts.cs
@@ -14,7 +14,7 @@ public class Hearts : MonoBehaviour {
 	void Update () {
 		
 	}
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Heart")
         {
--- GroupMobile/Assets/Scripts/NextLevel.cs
+++ GroupMobile/Assets/Scripts/NextLevel.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class NextLevel : MonoBehaviour
 {
     public string LeveltoLoad;
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         GameObject[] Hearts = GameObject.FindGameObjectsWithTag("Heart");
         if(collision.gameObject.tag == "Portal" && Hearts.Length == 0)
