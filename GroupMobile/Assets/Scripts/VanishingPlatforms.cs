af4b5a4f05a1be0438e2fa1c5bc15aa914866635
Hannah Baker <19.hannah.baker@ksd.org>
2019-03-07 10:28:43 -0800

    Testing VanishingPlatforms script in SampleScene

---
+++ GroupMobile/Assets/Scripts/VanishingPlatforms.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VanishingPlatforms : MonoBehaviour
+{
+    public GameObject[] timedPlatforms;
+    float timer = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer < 3f)
+        {
+            for (int i = 0; i < timedPlatforms.Length; i++)
+            {
+                (timedPlatforms[i]).SetActive(true);
+            }
+        }
+
+        if (timer > 3f)
+        {
+            for (int i = 0; i < timedPlatforms.Length; i++)
+            {
+                int r = Random.Range(0, 3);
+                if (r == 0)
+                {
+                    (timedPlatforms[i]).SetActive(false);
+                    break;
+                }
+
+                
+            }
+        }
+
+        if (timer > 6f)
+        {
+            timer = 0;
+        }
+    }
+}
